49de74879a659487204a7e51ed950dedc4dd806f
François Schmidts <francois.schmidts@gmail.com>
2017-11-10 18:35:16 +0100

    mouais

--- src/crawler/http_crawler.py
+++ src/crawler/http_crawler.py
@@ -181,7 +181,7 @@ async def crawl(username, password, **kwargs):
             continue
 
         # checking if cache was validated
-        a_im_support = feed.get('cache_support_a_im', False)
+        kwargs = {}
         if resp.status_code == 304:
             logger.info('feed responded with 304')
             clean_feed(feed, auth, no_resp_pool, resp,
@@ -189,7 +189,7 @@ async def crawl(username, password, **kwargs):
             continue
         elif resp.status_code == 226:
             logger.info('feed responded with 226')
-            a_im_support = True
+            kwargs['cache_support_a_im'] = True
         elif response_etag_match(feed, resp):
             clean_feed(feed, auth, no_resp_pool, resp,
                        cache_type=reasons.CacheReason.etag.value)
@@ -202,8 +202,7 @@ async def crawl(username, password, **kwargs):
             logger.debug('etag mismatch %r != %r',
                          resp.headers.get('etag'), feed.get('etag'))
 
-        result = challenge(feed, resp, auth, no_resp_pool,
-                           a_im_support=a_im_support)
+        result = challenge(feed, resp, auth, no_resp_pool, **kwargs)
         if result:
             all_entries, future = result
             challenge_pool.append((future, feed, all_entries))
