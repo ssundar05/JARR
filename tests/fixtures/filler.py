2bfba4c0d080af77380263ca6c9d8f6b35d7f119
François Schmidts <francois.schmidts@gmail.com>
2017-12-02 01:20:54 +0100

    wip

--- tests/fixtures/filler.py
+++ tests/fixtures/filler.py
@@ -7,10 +7,18 @@ from jarr.controllers import (ArticleController, CategoryController,
 from jarr.models import User, Category, Feed, Cluster, Article, Tag, Icon
 
 
+def to_name(user, iter_, cat=None, feed=None, art=None, *args):
+    string = "i%d %s" % (iter_, user.login)
+    if cat:
+        string += " cat%s" % cat
+    if feed is not None:
+        string += " feed%s" % feed
+    if art is not None:
+        string += " art%s" % art
+    return string + ''.join(args)
+
+
 def populate_db():
-    #session.query(Cluster).update({'main_article_id': None})
-    #for table in Tag, Article, Cluster, Feed, Icon, Category, User:
-    #    session.query(table).delete()
     ucontr = UserController()
     ccontr = CategoryController()
     fcontr = FeedController()
@@ -24,35 +32,30 @@ def populate_db():
                     for name in ["user1", "user2"]]
     now = utc_now()
 
-    article_total = 0
-    for k in range(2):
+    for iteration in range(2):
+        article_total = 0
 
-        def to_name(u, iter_, cat=None, feed=None, art=None, *args):
-            string = "i%d %s" % (iter_, u.login)
-            if cat:
-                string += " cat%s" % cat
-            if feed is not None:
-                string += " feed%s" % feed
-            if art is not None:
-                string += " art%s" % art
-            return string + ''.join(args)
         for user in (user1, user2):
-            for i in range(3):
+            for iter_cat in range(3):
                 cat_id = None
-                if i:
+                if iter_cat:
                     cat_id = ccontr.create(user_id=user.id,
-                                           name=to_name(user, k, i)).id
-                feed = fcontr.create(link="feed%d%d" % (k, i), user_id=user.id,
-                                     category_id=cat_id,
-                                     title=to_name(user, k, i, i))
-                for j in range(3):
-                    entry = to_name(user, k, i, i, j)
+                                           name=to_name(user, iteration,
+                                                        iter_cat)).id
+                feed = fcontr.create(link="feed%d%d" % (iteration, iter_cat),
+                                     user_id=user.id, category_id=cat_id,
+                                     title=to_name(user, iteration,
+                                                   iter_cat, iter_cat))
+                for iter_art in range(3):
+                    entry = to_name(user, iteration,
+                                    iter_cat, iter_cat, iter_art)
+
+                    tags = [to_name(user, iteration, iter_cat, iter_cat,
+                            iter_art, str(i)) for i in range(2)]
                     article_total += 1
                     acontr.create(entry_id=entry,
                             link='http://test.te/%d' % article_total,
-                            feed_id=feed.id, user_id=user.id,
-                            tags=[to_name(user, k, i, i, j, '1'),
-                                  to_name(user, k, i, i, j, '2')],
+                            feed_id=feed.id, user_id=user.id, tags=tags,
                             category_id=cat_id, title=entry,
-                            date=now + timedelta(seconds=k),
+                            date=now + timedelta(seconds=iteration),
                             content="content %d" % article_total)
